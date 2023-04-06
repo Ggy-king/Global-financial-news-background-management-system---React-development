b2ce5791e8066e077d89c492cd81fd4efd34656b
king <102906132+Ggy-king@users.noreply.github.com>
2023-04-06 22:26:48 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import IndexRouter from "./router/IndexRouter"
+import './App.css'
+
+function App() {
+  return <IndexRouter></IndexRouter>
+  
+}
+
+export default App
---
+++ src/views/newsSand/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { Button } from 'antd';
+
+
+export default function Home() {
+
+  return (
+    <div>
+      马上做
+    </div>
+  )
+}
